3d9691110b46da627b8646d5307857c09d3ad37e
IvanRengelFerreira <ivan.rengelferreira@usp.ceu.es>
2023-11-07 12:08:57 +0100

    final

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -5,13 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 public class Principal{
 
-    //DateFormat formatoEspañol = new SimpleDateFormat("d-MM-yyy, hh:mm:ss a ");
-        //String date = formatoEspañol.format(new Date());
+    public static void main(String[] args){
 
         ZonedDateTime caracas = ZonedDateTime.now(ZoneId.of("America/Caracas"));
         ZonedDateTime españa = ZonedDateTime.now();
 
-        DateTimeFormatter formatoEspañol = DateTimeFormatter.ofPattern("MM-dd-yyy, hh:mm:ss a");
+        DateTimeFormatter formatoEspañol = DateTimeFormatter.ofPattern("dd-MM-yyy, hh:mm:ss a");
 
          //Fecha actual caracas con  formato español
          String caracasFecha = caracas.format(formatoEspañol);
@@ -22,24 +21,21 @@ public class Principal{
         LocalDate añosAmerica = LocalDate.of(1492,Month.OCTOBER,12);
 
         //Diferencia de horas e y c
-        Duration duration = Duration.between(caracas,españa );
-        long diffHoras = duration.toHours();
+        int offsetCaracas = caracas.getOffset().getTotalSeconds();
+        int offsetSpain = españa.getOffset().getTotalSeconds();
+        // Calcular la diferencia en horas
+        int diffHoras = (offsetSpain - offsetCaracas) / 3600;
 
         //Fecha de cuando se piso la luna
         LocalDateTime luna = LocalDateTime.of(1969, Month.JULY, 11, 3, 56);
-
-
-    public void main(String[] args){
-
-        
         
         System.out.println("La fecha actual en España es "+ españaFecha );
         System.out.println("La fecha actual en Caracas es "+ caracasFecha );
-        System.out.println("El tiempo transcurrido desde el descubrimiento de america es de "+ ChronoUnit.YEARS.between(añosAmerica, españa) );
+        System.out.println("El tiempo transcurrido desde el descubrimiento de america es de "+ ChronoUnit.YEARS.between(añosAmerica, españa)+"años" );
         System.out.println("La hora actual en España es "+españa.toLocalTime());
         System.out.println("La hora actual en España es "+ caracas.toLocalTime());
         System.out.println("La diferencia de horas entre España y Caracas son "+ diffHoras);
-        System.out.println("La hora y feccha en el que Neil Amstring psio la Luna en hora de Madrid fue" +luna);
+        System.out.println("La hora y fecha en el que Neil Amstrong psio la Luna en hora de Madrid fue " +luna);
         
        
 
